345bcdbe8b151b2a9a71a5c04a3780e865157a68
LeoDNascimento <leonardo.nascimento1306@gmail.com>
2020-07-22 15:17:10 -0300

    Criando avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUsersService';
+import ensureAuth from '../middleware/ensureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
     try {
@@ -21,5 +25,15 @@ usersRouter.post('/', async (request, response) => {
         return response.status(400).json({ error: err.message });
     }
 });
+
+usersRouter.patch(
+    '/avatar',
+    ensureAuth,
+    upload.single('avatar'),
+    async (request, response) => {
+        console.log(request.file);
+        return response.json({ message: 'ok' });
+    },
+);
 
 export default usersRouter;
